f14a92853897e4888c9e85c04d4a36709e79725a
samWidlund <sam.wid415@gmail.com>
2025-08-07 23:53:31 +0200

    function returing true when user is online

--- web/script.js
+++ web/script.js
@@ -1,1 +1,24 @@
 console.log("script.js är laddad!");
+
+function checkOnlineStatus() {
+    if (navigator.onLine) {
+        console.log("Användaren är online");
+    } else {
+        console.log("Användaren är offline");
+    }
+}
+
+// check when the user is online or offline
+window.addEventListener('online', function() {
+    console.log("Anslutning återställd");
+    checkOnlineStatus();
+});
+window.addEventListener('offline', function() {
+    console.log("Anslutning förlorad");
+    checkOnlineStatus();
+});
+
+// check when the page is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    checkOnlineStatus();
+});
