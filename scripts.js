9495c2b2c54af7263b9847b33959ff94b584f7c4
samWidlund <sam.wid415@gmail.com>
2025-08-10 18:14:04 +0200

    implement working digital 24 hour clock

--- scripts.js
+++ scripts.js
@@ -22,3 +22,22 @@ window.addEventListener('offline', function() {
 document.addEventListener('DOMContentLoaded', function() {
     checkOnlineStatus();
 });
+
+
+// live clock
+const clock24 = document.getElementById('clock24')
+
+// Concatenate a zero to the left of every single digit time frame
+function concatZero(timeFrame) {
+  return timeFrame < 10 ? '0'.concat(timeFrame) : timeFrame
+}
+
+function realTime() {
+  let date = new Date()
+  let sec = date.getSeconds()
+  let mon = date.getMinutes()
+  let hr = date.getHours()
+  clock24.textContent = `${concatZero(hr)} : ${concatZero(mon)} : ${concatZero(sec)}`
+}
+
+setInterval(realTime, 1000)
